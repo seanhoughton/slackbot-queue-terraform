6338aaa73bba20215935f166b2f4449fd535e6ad
Sean Houghton <sean.houghton@activision.com>
2018-10-29 18:30:25 -0700

    Remove non-module files

--- providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
